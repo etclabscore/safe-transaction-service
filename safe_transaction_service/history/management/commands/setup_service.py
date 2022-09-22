b640c4fc6d71f63ab1b4181b152a12d12d678247
etccoop <team@etccooperative.org>
2022-09-22 21:37:56 +0000

    Add ETHEREUM_CLASSIC_MAINNET in setup_service

--- safe_transaction_service/history/management/commands/setup_service.py
+++ safe_transaction_service/history/management/commands/setup_service.py
@@ -194,6 +194,9 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 57070, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 57070, "1.3.0"),
     ],
+    EthereumNetwork.ETHEREUM_CLASSIC_MAINNET: [
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 15904946, "1.3.0"),
+    ],
     EthereumNetwork.ETHEREUM_CLASSIC_TESTNET_MORDOR: [
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 6333172, "1.3.0"),
     ],
@@ -340,6 +343,9 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     ],
     EthereumNetwork.ARBITRUM_TESTNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 57070),  # v1.3.0
+    ],
+    EthereumNetwork.ETHEREUM_CLASSIC_MAINNET: [
+        ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 15904946),  # v1.3.0
     ],
     EthereumNetwork.ETHEREUM_CLASSIC_TESTNET_MORDOR: [
         ("0x69f4D1788e39c87893C980c06EdF4b7f686e2938", 6333172),  # v1.3.0
